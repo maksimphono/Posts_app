6c76532ceeb1c87ab3f6b3f58058e6343669c1d8
Maxun <trukhinmaksimof@mail.ru>
2023-02-08 01:17:41 +0800

    styling POstItem component

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,11 +1,18 @@
 import React , {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import "./css/Navigation.scss";
+import home_icon from "../assets/icons/icons8-home.svg";
+import search_icon from "../assets/icons/icons8-search.svg";
+import user_icon from "../assets/icons/icons8-user-male.svg";
+import add_icon from "../assets/icons/icons8-plus.svg";
 
 function Navigation() {
   return (
     <nav>
-        <Link to = "/">Home</Link>
+        <Link to = "/"><img src={home_icon} alt="" /></Link>
+        <Link to = "/"><img src={search_icon} alt="" /></Link>
+        <Link to = "/"><img src={add_icon} alt="" /></Link>
+        <Link to = "/"><img src={user_icon} alt="" /></Link>
     </nav>
   )
 }
--- frontend/src/pages/HomePage/PostItem.jsx
+++ frontend/src/pages/HomePage/PostItem.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import styles from "./css/PostItem.module.scss";
+import home_icon from "../../assets/icons/icons8-home.svg";
+import search_icon from "../../assets/icons/icons8-search.svg"
 
 function PostItem({post}) {
   return (
     <div className={styles.post__item}>
-      <div className = {styles.post__header}>Freddy</div>
+      <div className = {styles.post__header}>
+        <img className="avatar" src={home_icon} alt="" />
+        Freddy
+      </div>
       <div className={styles.images}>
         <img src="https://th.bing.com/th/id/R.cf9eb0f2dabb9017eae919121bdff925?rik=y7xMZzy3bOXmXA&riu=http%3a%2f%2fwww.voyageurtripper.com%2fwp-content%2fuploads%2f2021%2f05%2fBest-Viewpoints-in-Vancouver-Jerico-Beach-3.jpg&ehk=LhvugNm03B9Yc6PInxnjlErtgO8d0R7Ty79wt8mwivQ%3d&risl=&pid=ImgRaw&r=0" alt="" />
       </div>
       <div className={styles.content}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis distinctio quisquam magni eaque incidunt nulla laboriosam rerum eveniet. Cum libero laboriosam molestiae sapiente distinctio voluptate ratione fuga amet dolore.
       </div>
-      <div className={styles.footer}></div>
+      <div className={styles.footer}>
+        <img src={home_icon} alt="" />
+        <img src={search_icon} alt="" />
+        <img src={search_icon} alt="" />
+      </div>
     </div>
   )
 }
